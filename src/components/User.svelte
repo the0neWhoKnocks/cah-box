0d2541f6b72303f128ad942be59a93cc0ec99049
Nox <the0neWhoKnocks@users.noreply.github.com>
2020-08-03 15:31:10 -0700

    feat: Refined User UI
    - Created a User component to handle the different User UI states.
    - Created Modal component to centralize all Modal logic.

---
+++ src/components/User.svelte
@@ -0,0 +1,55 @@
+<style>
+  .user {
+    font-size: 1.25em;
+    display: flex;
+  }
+
+  .user__icon,
+  .user__status-indicator,
+  .user__name {
+    line-height: 1em;
+    padding: 0.25em;
+    display: inline-block;
+  }
+
+  .user__name {
+    width: 100%;
+  }
+
+  .user:not(.is--admin) .user__icon {
+    opacity: 0;
+  }
+
+  .user__status-indicator {
+    background-color: #27cfb6;
+    transition: background-color 300ms;
+  }
+  .user:not(.is--active) .user__status-indicator {
+    background-color: #393939;
+  }
+</style>
+
+<script>
+  const ICON__ADMIN = '&#x2605;';
+  const ICON__CZAR = '&#x1F451;';
+  let className = '';
+  
+  export let active = false;
+  export let admin = false;
+  export let czar = false;
+  export let name = '';
+  export { className as class };
+
+  const icon = czar ? ICON__CZAR : ICON__ADMIN;
+</script>
+
+<div
+  class={`user ${className}`}
+  class:is--active={active}
+  class:is--admin={admin}
+  class:is--czar={czar}
+>
+  <span class="user__icon">{@html icon}</span>
+  <span class="user__name">{name}</span>
+  <span class="user__status-indicator"></span>
+</div>
--- src/server/socket/handlers/enterRoom.js
+++ src/server/socket/handlers/enterRoom.js
@@ -1,8 +1,11 @@
-module.exports = (socket) => function enterRoom({ roomID }) {
+module.exports = (socket) => function enterRoom({ roomID, username }) {
   const { WS_MSG__ENTER_ROOM } = require('../../../constants');
   const { rooms } = require('../store');
 
   socket.join(roomID, () => {
-    socket.emit(WS_MSG__ENTER_ROOM, rooms[roomID]);
+    socket.emit(WS_MSG__ENTER_ROOM, {
+      roomData: rooms[roomID],
+      username,
+    });
   });
 }
