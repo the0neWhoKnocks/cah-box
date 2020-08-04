47390fcdd188bbabf176cea3ea1d0799e2d249b1
tlemon <trevor.lemon@nike.com>
2020-08-03 19:43:10 -0700

    feat: Czar & Admin assignment
    The Admin can now assign the Czar or assign Admin to another User.

--- src/server/socket/handlers/connection.js
+++ src/server/socket/handlers/connection.js
@@ -4,14 +4,19 @@ module.exports = function connection(socket) {
     WS_MSG__CREATE_GAME,
     WS_MSG__ENTER_ROOM,
     WS_MSG__JOIN_GAME,
+    WS_MSG__SET_ADMIN,
+    WS_MSG__SET_CZAR,
   } = require('../../../constants');
   const checkUsername = require('./checkUsername');
   const createGame = require('./createGame');
   const enterRoom = require('./enterRoom');
   const joinGame = require('./joinGame');
+  const setUserState = require('./setUserState');
 
   socket.on(WS_MSG__CHECK_USERNAME, checkUsername(socket));
   socket.on(WS_MSG__CREATE_GAME, createGame(socket));
   socket.on(WS_MSG__ENTER_ROOM, enterRoom(socket));
   socket.on(WS_MSG__JOIN_GAME, joinGame(socket));
+  socket.on(WS_MSG__SET_ADMIN, setUserState(socket, 'admin'));
+  socket.on(WS_MSG__SET_CZAR, setUserState(socket, 'czar'));
 }
---
+++ src/server/socket/handlers/setUserState.js
@@ -0,0 +1,14 @@
+module.exports = (socket, prop) => function setUserState({ roomID, username }) {
+  const { WS_MSG__USER_UPDATE } = require('../../../constants');
+  const { io, rooms } = require('../store');
+
+  for (let i = 0; i < rooms[roomID].users.length; i++) {
+    const user = rooms[roomID].users[i];
+
+    user[prop] = user.name === username;
+  }
+  
+  io.sockets.in(roomID).emit(WS_MSG__USER_UPDATE, {
+    users: rooms[roomID].users,
+  });
+}
