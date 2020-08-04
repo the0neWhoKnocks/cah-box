b2c8a1de95c033602c0bf4830de5539fad8c429c
tlemon <trevor.lemon@nike.com>
2020-08-04 14:09:15 -0700

    feat: Card are dealt
    - Added events to deal cards to all Users.
    - Cards display on Mobile & Desktop

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
 module.exports = {
+  WS_MSG__CARDS_DEALT: 'cards dealt',
   WS_MSG__CHECK_USERNAME: 'check username',
   WS_MSG__CREATE_GAME: 'create game',
+  WS_MSG__DEAL_CARDS: 'deal cards',
   WS_MSG__ENTER_ROOM: 'enter room',
   WS_MSG__JOIN_GAME: 'join game',
   WS_MSG__SET_ADMIN: 'set admin',
--- src/server/socket/handlers/connection.js
+++ src/server/socket/handlers/connection.js
@@ -2,6 +2,7 @@ module.exports = function connection(socket) {
   const {
     WS_MSG__CHECK_USERNAME,
     WS_MSG__CREATE_GAME,
+    WS_MSG__DEAL_CARDS,
     WS_MSG__ENTER_ROOM,
     WS_MSG__JOIN_GAME,
     WS_MSG__SET_ADMIN,
@@ -9,12 +10,14 @@ module.exports = function connection(socket) {
   } = require('../../../constants');
   const checkUsername = require('./checkUsername');
   const createGame = require('./createGame');
+  const dealCards = require('./dealCards');
   const enterRoom = require('./enterRoom');
   const joinGame = require('./joinGame');
   const setUserState = require('./setUserState');
 
   socket.on(WS_MSG__CHECK_USERNAME, checkUsername(socket));
   socket.on(WS_MSG__CREATE_GAME, createGame(socket));
+  socket.on(WS_MSG__DEAL_CARDS, dealCards(socket));
   socket.on(WS_MSG__ENTER_ROOM, enterRoom(socket));
   socket.on(WS_MSG__JOIN_GAME, joinGame(socket));
   socket.on(WS_MSG__SET_ADMIN, setUserState(socket, 'admin'));
