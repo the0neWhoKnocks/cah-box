a31647da9d856b4aa84c11f47736969dd7157ab6
tlemon <trevor.lemon@nike.com>
2020-08-05 15:34:41 -0700

    feat: User's can select cards
    - A User can pick the required amount of cards.
    - Once the required number of cards is chosen, the remaining cards will become disabled.
    - A User can click the chosen card, to un-choose it.

--- src/server/socket/handlers/dealCards.js
+++ src/server/socket/handlers/dealCards.js
@@ -1,20 +1,30 @@
+const getRequiredCount = (blackCard) => {
+  const numOfGaps = (blackCard.match(/(_+)/g) || []).length;
+  return (numOfGaps > 1) ? numOfGaps : 1;
+};
+
 module.exports = () => function dealCards({ roomID }) {
   const { WS_MSG__CARDS_DEALT } = require('../../../constants');
   const { io, rooms } = require('../store');
   const { cards, users } = rooms[roomID];
   const { live } = cards;
   const MAX_CARDS = 10;
+  const blackCard = live.black.shift();
   
   for (let i = 0; i < MAX_CARDS; i++) {
     for (let j = 0; j < users.length; j++) {
       const user = users[j];
 
-      if (user.cards.length < MAX_CARDS) user.cards.push(live.white.shift());
+      if (user.cards.length < MAX_CARDS) user.cards.push({
+        selected: false,
+        text: live.white.shift(),
+      });
     }
   }
 
   io.sockets.in(roomID).emit(WS_MSG__CARDS_DEALT, {
-    blackCard: live.black.shift(),
+    blackCard,
+    requiredWhiteCardsCount: getRequiredCount(blackCard), 
     users,
   });
 }
