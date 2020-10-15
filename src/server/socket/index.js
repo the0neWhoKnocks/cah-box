98e8d8eee1bded2b64cbabf6f172b578c67eeafb
Trevor Lemon <duhlemman@gmail.com>
2020-10-14 17:48:25 -0700

    feat: Ensure room is torn
    - Rooms will no longer sit in Server memory if a User creates a room but no one joins, including the User that created it.
    - Moved the `close` handler for the socket out of `enterRoom` to handle all `close` cases in one place at the highest level so I don't miss any cases.

--- src/server/socket/index.js
+++ src/server/socket/index.js
@@ -1,6 +1,11 @@
 const { OPEN, Server } = require('ws');
 const log = require('../../utils/logger')('socket');
+const getUser = require('../utils/getUser');
+
 const rooms = new Map();
+const disconnectChecksForRoom = new Map();
+
+const disconnectKey = (roomID, username) => `${roomID}__${username}`;
 
 // NOTE - wrapping API in case I need to refactor again in the future
 class ServerSocket {
@@ -69,24 +74,51 @@ class ServerSocket {
     }
   }
 
+  enterRoom(roomID, username, cb) {
+    const room = rooms.get(roomID);
+
+    if (room) room.sockets.push(this.socket);
+    // User didn't create the room, they're just joining so keep a reference
+    if (!this.data.roomID) this.data.roomID = roomID;
+
+    // Rejoining a running game
+    if (room && username) {
+      const user =  getUser(room, username);
+      
+      if (user) {
+        this.data.user = user;
+        user.connected = true;
+        
+        const dKey = disconnectKey(roomID, username);
+        const disconnectCheck = disconnectChecksForRoom.get(dKey);
+        if (disconnectCheck) {
+          clearTimeout(disconnectCheck);
+          disconnectChecksForRoom.delete(dKey);
+        }
+  
+        log(`User "${user.name}" reconnected`);
+      }
+    }
+    
+    cb(room);
+  }
+
   getRoom(roomID) {
     return rooms.get(roomID);
   }
 
-  joinRoom(roomID, cb) {
-    const room = rooms.get(roomID);
-    if (room) room.sockets.push(this.socket);
-    cb(room);
+  leaveRoom(roomID, username, timeout) {
+    disconnectChecksForRoom.set(disconnectKey(roomID, username), timeout);
   }
 }
 
 module.exports = function socket(server) {
   const wss = new Server({ server });
-  const connectionHandlers = require('./connectionHandlers');
+  const socketHandlers = require('./socketHandlers');
 
   wss.on('connection', function connected(currentUserSocket) {
     log('Server Socket connected to Client');
-    connectionHandlers(currentUserSocket, new ServerSocket(currentUserSocket, wss));
+    socketHandlers(currentUserSocket, new ServerSocket(currentUserSocket, wss));
   });
 
   return new ServerSocket(undefined, wss);
